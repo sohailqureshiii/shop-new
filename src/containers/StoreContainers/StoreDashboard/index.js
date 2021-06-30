12966733fe42965f3a6f1a2cab83daf8342e66f1
rahul <rahulc2303@gmail.com>
2021-06-30 11:34:00 +0530

    Dashboard

---
+++ src/containers/StoreContainers/StoreDashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashBoard from '../../../components/DashBoard'
+
+const StoreDasboard = () => {
+    return (
+        <DashBoard sidebar>
+         Store Home
+      </DashBoard>
+    )
+}
+
+export default StoreDasboard
