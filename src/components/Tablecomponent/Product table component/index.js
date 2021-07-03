7ad9b514ee7783a83f3d4c51f104200e5908fdca
sohailqureshiii <sohail.qureshi@adypu.edu.in>
2021-07-03 12:28:34 +0530

    store order page and product page

---
+++ src/components/Tablecomponent/Product table component/index.js
@@ -0,0 +1,136 @@
+import React from 'react'
+
+const  ProductTableCompo = (props) => {
+    return (
+        <table className="table-new-table">
+            <thead className="oknbhgrtyfc">
+              <th className="jfkvvjvsv kjilljjhn">S.No</th>
+              <th className="jfkvvjvsv kjilljjhn">Product Id</th>
+              <th className="jfkvvjvsv kjilljjhn">Product Name</th>
+              <th className="jfkvvjvsv kjilljjhn">Quantity</th>
+              <th className="jfkvvjvsv kjilljjhn">Amount</th>
+              <th className="jfkvvjvsv kjilljjhn">Items</th>
+              <th className="jfkvvjvsv kjilljjhn">Stock</th>
+            </thead>
+            <tbody className="lgadkyhdtq">
+              <tr className="mnbnmnb">
+                <td className="jfkvvjvsv" data-label="S.No">
+                  2
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Id">
+                  A1-OKM-098756
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Name">
+                  Apple Mac Book
+                </td>
+                <td className="jfkvvjvsv" data-label="Quantity">
+                  345
+                </td>
+                <td className="jfkvvjvsv" data-label="Amount">
+                  $200
+                </td>
+                <td className="jfkvvjvsv" data-label="Items">
+                  678 items
+                </td>
+                <td className="jfkvvjvsv" data-label="Stock">
+                  Available
+                </td>
+              </tr>
+              <tr className="mnbnmnb">
+                <td className="jfkvvjvsv" data-label="S.No">
+                  3
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Id">
+                  A1-OKM-09834523
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Name">
+                  Samsung M10 New Phone
+                </td>
+                <td className="jfkvvjvsv" data-label="Quantity">
+                  345
+                </td>
+                <td className="jfkvvjvsv" data-label="Amount">
+                  $80
+                </td>
+                <td className="jfkvvjvsv" data-label="Items">
+                  678 items
+                </td>
+                <td className="jfkvvjvsv" data-label="Stock">
+                  Available
+                </td>
+              </tr>
+              <tr className="mnbnmnb">
+                <td className="jfkvvjvsv" data-label="S.No">
+                  4
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Id">
+                  A1-OKM-0989807
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Name">
+                  Realmi New Brand Mobile
+                </td>
+                <td className="jfkvvjvsv" data-label="Quantity">
+                  345
+                </td>
+                <td className="jfkvvjvsv" data-label="Amount">
+                  $20
+                </td>
+                <td className="jfkvvjvsv" data-label="Items">
+                  678 items
+                </td>
+                <td className="jfkvvjvsv" data-label="Stock">
+                  Available
+                </td>
+              </tr>
+              <tr className="mnbnmnb">
+                <td className="jfkvvjvsv" data-label="S.No">
+                  5
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Id">
+                  A1-OKM-098345
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Name">
+                2020 Apple MacBook Air - Gold
+                </td>
+                <td className="jfkvvjvsv" data-label="Quantity">
+                  345
+                </td>
+                <td className="jfkvvjvsv" data-label="Amount">
+                  $2100
+                </td>
+                <td className="jfkvvjvsv" data-label="Items">
+                  678 items
+                </td>
+                <td className="jfkvvjvsv" data-label="Stock">
+                  Available
+                </td>
+              </tr>
+              <tr className="mnbnmnb">
+                <td className="jfkvvjvsv" data-label="S.No">
+                  6
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Id">
+                  A1-OKM-0987656
+                </td>
+                <td className="jfkvvjvsv" data-label="Product Name">
+               1 Plus Airpods - Black 
+                </td>
+                <td className="jfkvvjvsv" data-label="Quantity">
+                  345
+                </td>
+                <td className="jfkvvjvsv" data-label="Amount">
+                  $2100
+                </td>
+                <td className="jfkvvjvsv" data-label="Items">
+                  678 items
+                </td>
+                <td className="jfkvvjvsv" data-label="Stock">
+                  Available
+                </td>
+              </tr>
+            </tbody>
+          </table>
+    )
+}
+
+export default ProductTableCompo;
--- src/containers/StoreContainers/StoreOrder/index.js
+++ src/containers/StoreContainers/StoreOrder/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import DashBoard from "../../../components/DashBoard";
 import Navigationbar from "../../../components/Navbar";
+import SeachCompo from "../../../components/SearchComponent";
+import OrderTableCompo from "../../../components/Tablecomponent/Order table component";
 import './style.css'
 
 const StoreOrder = () => {
@@ -9,58 +11,10 @@ const StoreOrder = () => {
  
       <DashBoard sidebar>
       <div className='lkjhasdrfr'>
-        <h1 className='order-name-name-order'>
-          Orders
-        </h1>
-        <table className="table-new-table">
-          <thead className='oknbhgrtyfc'>
-            <th className="jfkvvjvsv kjilljjhn" >S.No</th>
-            <th className="jfkvvjvsv kjilljjhn" >Order Id</th>
-            <th className="jfkvvjvsv kjilljjhn" >Customer</th>
-            <th className="jfkvvjvsv kjilljjhn" >Date / Time</th>
-            <th className="jfkvvjvsv kjilljjhn" >Amount</th>
-            <th className="jfkvvjvsv kjilljjhn" >Items</th>
-            <th className="jfkvvjvsv kjilljjhn" >Status</th>
-          </thead>
-          <tbody className='lgadkyhdtq'>
-            <tr className='mnbnmnb'>
-              <td className="jfkvvjvsv" data-label="S.No">1</td>
-              <td className="jfkvvjvsv"  data-label="Order Id">A1-OKM-098756</td>
-              <td className="jfkvvjvsv"  data-label="Customer">Sohail</td>
-              <td className="jfkvvjvsv"  data-label="Date / Time">18 Oct 2021</td>
-              <td className="jfkvvjvsv"  data-label="Amount">$200</td>
-              <td className="jfkvvjvsv"  data-label="Items">3 items</td>
-              <td className="jfkvvjvsv"  data-label="Status">Pending</td>
-            </tr>
-            <tr className='mnbnmnb'>
-              <td className="jfkvvjvsv"  data-label="S.No">2</td>
-              <td className="jfkvvjvsv"  data-label="Order Id">A1-OKM-098756</td>
-              <td className="jfkvvjvsv"  data-label="Customer">Rahul</td>
-              <td className="jfkvvjvsv"  data-label="Date / Time">08 Oct 2021</td>
-              <td className="jfkvvjvsv"  data-label="Amount">$200</td>
-              <td className="jfkvvjvsv"  data-label="Items">9 items</td>
-              <td className="jfkvvjvsv"  data-label="Status">Shipping</td>
-            </tr>
-            <tr className='mnbnmnb'>
-              <td className="jfkvvjvsv"  data-label="S.No">3</td>
-              <td className="jfkvvjvsv"  data-label="Order Id">A1-OKM-098756</td>
-              <td className="jfkvvjvsv"  data-label="Customer">Sameer</td>
-              <td className="jfkvvjvsv"  data-label="Date / Time">08 Oct 2021</td>
-              <td className="jfkvvjvsv"  data-label="Amount">$200</td>
-              <td className="jfkvvjvsv"  data-label="Items">5 items</td>
-              <td className="jfkvvjvsv"  data-label="Status">Completed</td>
-            </tr>
-            <tr className='mnbnmnb'>
-              <td className="jfkvvjvsv"  data-label="S.No">3</td>
-              <td className="jfkvvjvsv"  data-label="Order Id">A1-OKM-098756</td>
-              <td className="jfkvvjvsv"  data-label="Customer">Sameer</td>
-              <td className="jfkvvjvsv"  data-label="Date / Time">08 Oct 2021</td>
-              <td className="jfkvvjvsv"  data-label="Amount">$200</td>
-              <td className="jfkvvjvsv"  data-label="Items">5 items</td>
-              <td className="jfkvvjvsv"  data-label="Status">Completed</td>
-            </tr> 
-          </tbody>
-        </table>
+      <h1 className="order-name-name-order">Orders
+          <SeachCompo/>
+          </h1>
+        <OrderTableCompo/>
       </div>
 
         </DashBoard> 
